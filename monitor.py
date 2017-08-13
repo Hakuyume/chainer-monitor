afbe7c50b9abbc2d8c2d93ce4d6d5ab5b08bddfb
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-13 22:01:03 +0900

    make a class

--- monitor.py
+++ monitor.py
@@ -10,110 +10,116 @@ from common import connect_db
 from common import gen_id
 
 
-conn = connect_db()
+class Monitor(bottle.Bottle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.conn = connect_db()
+
+        self.get('/api/logs', callback=self.get_logs)
+        self.get('/api/logs/<id_>', callback=self.get_log)
+
+        self.post('/api/plots', callback=self.new_plot)
+        self.get('/api/plots/<id_>', callback=self.get_plot)
+
+        self.route('/<filepath>', callback=self.static)
+        self.route('/root', callback=self.root)
+        self.route('/plots/<id_>', callback=self.plot)
+
+    def get_logs(self):
+        cur = self.conn.cursor()
+        return {
+            id_: {'path': path, 'comment': comment}
+            for (id_, path, comment) in cur.execute(r'SELECT * FROM logs')}
+
+    def get_log(self, id_):
+        cur = self.conn.cursor()
+        cur.execute(r'SELECT * FROM logs WHERE id=?', (id_,))
+
+        l = cur.fetchone()
+        if l is None:
+            return bottle.HTTPResponse(status=404)
+
+        _, path, comment = l
+
+        try:
+            content = json.load(open(path))
+        except FileNotFoundError:
+            content = list()
+
+        content = [
+            {k: (v if math.isfinite(v) else None)
+             for k, v in entry.items()}
+            for entry in content]
+
+        return {
+            'id': id_,
+            'path': path,
+            'comment': comment,
+            'content': content}
+
+    def new_plot(self):
+        cur = self.conn.cursor()
+
+        params = self.request.params
+
+        id_ = gen_id()
+        if 'comment' in params:
+            comment = params['comment']
+        else:
+            comment = ''
+
+        cur.execute(r'INSERT INTO plots VALUES(?,?)', (id_, comment))
+        self.conn.commit()
+
+        return {
+            'id': id_,
+            'comment': comment,
+            'series': dict()}
+
+    def get_plot(self, id_):
+        cur = self.conn.cursor()
+        cur.execute(r'SELECT * FROM plots WHERE id=?', (id_,))
+
+        p = cur.fetchone()
+        if p is None:
+            return bottle.HTTPResponse(status=404)
+
+        _, comment = p
+
+        series = {
+            id_: {'log': log, 'color': color}
+            for (id_, _, log, color) in
+            cur.execute(r'SELECT * FROM series WHERE plot=?', (id_,))}
+
+        return {
+            'id': id_,
+            'comment': comment,
+            'series': series}
+
+    def static(self, filepath):
+        return bottle.static_file(
+            filepath + '.html',
+            root=os.path.join(os.path.dirname(__file__), 'static'))
+
+    def plot(self, id_):
+        return self.static('plot')
+
+    def root(self):
+        r = bottle.HTTPResponse(status=302)
+        r.set_header('Location', 'logs')
+        return r
 
 
-@bottle.get('/api/logs')
-def get_logs():
-    cur = conn.cursor()
-    return {
-        id_: {'path': path, 'comment': comment}
-        for (id_, path, comment) in cur.execute(r'SELECT * FROM logs')}
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=8080)
+    args = parser.parse_args()
+
+    app = Monitor()
+    app.run(port=args.port)
 
 
-@bottle.get('/api/logs/<id_>')
-def get_log(id_):
-    cur = conn.cursor()
-    cur.execute(r'SELECT * FROM logs WHERE id=?', (id_,))
-
-    l = cur.fetchone()
-    if l is None:
-        return bottle.HTTPResponse(status=404)
-
-    _, path, comment = l
-
-    try:
-        content = json.load(open(path))
-    except FileNotFoundError:
-        content = list()
-
-    content = [
-        {k: (v if math.isfinite(v) else None) for k, v in entry.items()}
-        for entry in content]
-
-    return {
-        'id': id_,
-        'path': path,
-        'comment': comment,
-        'content': content}
-
-
-@bottle.get('/api/plots/<id_>')
-def get_plot(id_):
-    cur = conn.cursor()
-    cur.execute(r'SELECT * FROM plots WHERE id=?', (id_,))
-
-    p = cur.fetchone()
-    if p is None:
-        return bottle.HTTPResponse(status=404)
-
-    _, comment = p
-
-    series = {
-        id_: {'log': log, 'color': color}
-        for (id_, _, log, color) in
-        cur.execute(r'SELECT * FROM series WHERE plot=?', (id_,))}
-
-    return {
-        'id': id_,
-        'comment': comment,
-        'series': series}
-
-
-@bottle.post('/api/plots')
-def new_plot():
-    cur = conn.cursor()
-
-    params = bottle.request.params
-
-    id_ = gen_id()
-    if 'comment' in params:
-        comment = params['comment']
-    else:
-        comment = ''
-
-    cur.execute(r'INSERT INTO plots VALUES(?,?)', (id_, comment))
-    conn.commit()
-
-    return {
-        'id': id_,
-        'comment': comment,
-        'series': dict()}
-
-
-@bottle.get('/<filepath>')
-def static(filepath):
-    return bottle.static_file(
-        filepath + '.html',
-        root=os.path.join(os.path.dirname(__file__), 'static'))
-
-
-@bottle.get('/plots/<id_>')
-def static_plot(id_):
-    return bottle.static_file(
-        'plot.html',
-        root=os.path.join(os.path.dirname(__file__), 'static'))
-
-
-@bottle.get('/')
-def root():
-    r = bottle.HTTPResponse(status=302)
-    r.set_header('Location', 'logs')
-    return r
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--port', type=int, default=8080)
-args = parser.parse_args()
-
-bottle.run(port=args.port)
+if __name__ == '__main__':
+    main()
