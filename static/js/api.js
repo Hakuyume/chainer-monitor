b83c75ea96fd3251b0d77183c65c3d639b4c28a9
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-14 23:48:33 +0900

    simplify js

--- static/js/api.js
+++ static/js/api.js
@@ -40,3 +40,56 @@ Logs.prototype.remove = function(log, callback) {
         }
     });
 };
+
+function Plot(id, comment) {
+    this.id = id;
+    this.comment = comment;
+};
+
+function Plots() {
+    this.plots = [];
+};
+
+Plots.prototype.fetch = function(callback) {
+    var self = this;
+
+    $.getJSON('/api/plots', function(res) {
+        self.plots = [];
+        $.each(res, function(id, plot) {
+            self.plots.push(new Plot(id, plot.comment));
+        });
+        callback();
+    });
+};
+
+Plots.prototype.each = function(callback) {
+    var self = this;
+
+    $.each(self.plots, function(_, plot) {
+        callback(plot);
+    });
+};
+
+Plots.prototype.add = function(plot, callback) {
+    var self = this;
+
+    $.post(
+        '/api/plots',
+        $.param({'comment': plot.comment}),
+        function() {
+            self.fetch(callback);
+        }
+    );
+};
+
+Plots.prototype.remove = function(plot, callback) {
+    var self = this;
+
+    $.ajax({
+        url: '/api/plots/' + plot.id,
+        type: 'DELETE',
+        success: function() {
+            self.fetch(callback);
+        }
+    });
+};
