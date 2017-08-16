37a0074da82f624e38847cf8b1ea9fd47afedd88
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-16 23:57:16 +0900

    update deploy

--- deploy.bash
+++ deploy.bash
@@ -2,23 +2,37 @@
 set -eu
 
 origin=$(cd $(dirname $0); pwd)
-commit=$(git rev-parse master)
 
 tempdir=$(mktemp -d)
 trap "rm -rf $tempdir" EXIT
 cd $tempdir
 
-npm install -D babel-preset-es2015
-git clone $origin gh-pages -b gh-pages
-cd gh-pages
+git clone $origin repo
+cd repo
 git config user.name 'anonymous'
 git config user.email ''
-git checkout origin/master -- static/
-git reset static/*
-mv -v static/*.html .
-mv -v static/js/{dummy-,}api.js
+
+git read-tree --empty
+
+for target in static/*.html
+do
+    key=$(git hash-object -w $target)
+    git update-index --add --cacheinfo 100644 $key $(basename $target)
+done
+
 perl -pe "s{'./plot\?'}{'./plot.html?'}" -i static/js/index.js
-babel static/js -d js --presets es2015 --compact true
-git add *.html js/*
-git commit -m "from $commit"
-git push origin gh-pages
+mv -v static/js/{dummy-,}api.js
+mkdir -p babel browserify
+babel static/js -d babel --presets es2015 --compact true
+for target in index.js plot.js
+do
+    NODE_PATH=babel browserify babel/$target -o browserify/$target
+    key=$(git hash-object -w browserify/$target)
+    git update-index --add --cacheinfo 100644 $key js/$target
+done
+
+tree=$(git write-tree)
+parent=$(git rev-parse origin/gh-pages)
+comment="from $(git rev-parse origin/master)"
+commit=$(echo $comment | git commit-tree $tree -p $parent)
+git push origin $commit:gh-pages
