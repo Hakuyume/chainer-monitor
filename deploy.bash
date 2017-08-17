dd81f5203acc0a5e684ade422fcc395d505ea896
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-17 10:25:46 +0900

    enable source map

--- deploy.bash
+++ deploy.bash
@@ -35,7 +35,7 @@ export NODE_PATH="${NODE_PATH:-}:static/js"
 for target in {index,plot}.js
 do
     perl -0pe 's{^}{import "babel-polyfill";}' -i static/js/$target
-    browserify static/js/$target -o $target \
+    browserify static/js/$target -o $target -d \
                -g uglifyify -t [ babelify --presets [ es2015 ] ]
     key=$(git hash-object -w $target)
     git update-index --add --cacheinfo 100644 $key js/$target
@@ -67,7 +67,7 @@ export NODE_PATH="${NODE_PATH:-}:static/js"
 for target in {index,plot}.js
 do
     perl -0pe 's{^}{import "babel-polyfill";}' -i static/js/$target
-    browserify static/js/$target -o $target \
+    browserify static/js/$target -o $target -d \
                -g uglifyify -t [ babelify --presets [ es2015 ] ]
     key=$(git hash-object -w $target)
     git update-index --add --cacheinfo 100644 $key static/js/$target
