66e404cca42988239e7185d48ddbb6bd9011f551
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-17 12:40:03 +0900

    update deploy tool

--- deploy.bash
+++ deploy.bash
@@ -1,80 +1,80 @@
 #! /usr/bin/env bash
 set -eu
 
-origin=$(cd $(dirname $0); pwd)
-sha256sum -c dummy_data/checksum
+export GIT_AUTHOR_NAME=anonymous
+export GIT_AUTHOR_EMAIL=
+export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 
-tempdir=$(mktemp -d)
-trap "rm -rf $tempdir" EXIT
-cd $tempdir
+project=$(cd $(dirname $0); pwd)
+cd $project
+sha256sum -c dummy_data/checksum
+rm -rf $project/dist
+
+builddir=$(mktemp -d)
+trap "rm -rf $builddir" EXIT
+cd $builddir
 npm install \
     browserify \
     babelify \
     babel-preset-es2015 \
     babel-polyfill \
     uglifyify
-
-git clone $origin repo
-cd repo
-git config user.name 'anonymous'
-git config user.email ''
+mkdir js
+export NODE_PATH="${NODE_PATH:-}:js"
 
 # gh-pages
-git reset --hard
-git read-tree --empty
+dist=dist/gh-pages
+mkdir -p $dist $dist/{api,js}
 
-for target in static/*.html
-do
-    key=$(git hash-object -w $target)
-    git update-index --add --cacheinfo 100644 $key $(basename $target)
-done
+cp $project/static/*.html $dist/
+cp $project/dummy_data/{log,plot}.json $dist/api/
 
-perl -pe "s{'./plot\?'}{'./plot.html?'}" -i static/js/index.js
-mv -v static/js/{dummy-,}api.js
-export NODE_PATH="${NODE_PATH:-}:static/js"
+cp $project/static/js/*.js js/
+perl -pe "s{'./plot\?'}{'./plot.html?'}" -i js/index.js
+mv js/{dummy-,}api.js
 for target in {index,plot}.js
 do
-    perl -0pe 's{^}{import "babel-polyfill";}' -i static/js/$target
-    browserify static/js/$target -o $target \
+    perl -0pe 's{^}{import "babel-polyfill";}' -i js/$target
+    browserify js/$target -o $dist/js/$target \
                -g uglifyify -t [ babelify --presets [ es2015 ] ]
-    key=$(git hash-object -w $target)
-    git update-index --add --cacheinfo 100644 $key js/$target
 done
-
-for target in {log,plot}.json
-do
-    key=$(git hash-object -w $origin/dummy_data/$target)
-    git update-index --add --cacheinfo 100644 $key api/$target
-done
-
-tree=$(git write-tree)
-parent=$(git rev-parse origin/gh-pages)
-comment="from $(git rev-parse origin/master)"
-commit=$(echo $comment | git commit-tree $tree -p $parent)
-git push origin $commit:gh-pages
 
 # release
-git reset --hard
-git read-tree --empty
+dist=dist/release
+mkdir -p $dist $dist/static $dist/static/js
 
-for target in README.md *.py static/*.html
-do
-    key=$(git hash-object -w $target)
-    git update-index --add --cacheinfo 100644 $key $target
-done
+cp $project/README.md $dist/
+cp $project/*.py $dist/
+cp $project/static/*.html $dist/static/
 
-export NODE_PATH="${NODE_PATH:-}:static/js"
+cp $project/static/js/*.js js/
 for target in {index,plot}.js
 do
-    perl -0pe 's{^}{import "babel-polyfill";}' -i static/js/$target
-    browserify static/js/$target -o $target \
+    perl -0pe 's{^}{import "babel-polyfill";}' -i js/$target
+    browserify js/$target -o $dist/static/js/$target \
                -g uglifyify -t [ babelify --presets [ es2015 ] ]
-    key=$(git hash-object -w $target)
-    git update-index --add --cacheinfo 100644 $key static/js/$target
 done
 
-tree=$(git write-tree)
-parent=$(git rev-parse origin/release)
-comment="from $(git rev-parse origin/master)"
-commit=$(echo $comment | git commit-tree $tree -p $parent)
-git push origin $commit:release
+cp -r dist $project/
+
+cd $project
+if git diff-index --quiet HEAD -- && git ls-files . --exclude-standard --others | grep ''; then
+    cd dist
+    for branch in *
+    do
+        git read-tree --empty
+        for file in $(find $branch -type f -printf '%P ')
+        do
+            key=$(git hash-object -w $branch/$file)
+            git update-index --add --cacheinfo 100644 $key $file
+        done
+        tree=$(git write-tree)
+        parent=$(git rev-parse $branch)
+        comment="from $(git rev-parse HEAD)"
+        commit=$(git commit-tree $tree -p $parent -m "$comment")
+        git branch -f $branch $commit
+    done
+
+    git reset
+fi
