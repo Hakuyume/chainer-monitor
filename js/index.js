b1cc64a2b071ef75842be006cf58d0c608e6cce6
anonymous <>
2017-08-18 12:07:51 +0900

    from 8e782a0150293f8ee433b2648f0bd02dfec80010

--- js/index.js
+++ js/index.js
@@ -327,100 +327,333 @@ exports.default = Monitor;
 },{}],2:[function(require,module,exports){
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _api = require('api');
 
 var _api2 = _interopRequireDefault(_api);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var monitor = new _api2.default('./api');
 
-function update_plots() {
-    var tbody = $('#plots > tbody');
-    tbody.empty();
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
+var Button = ReactBootstrap.Button;
+var Clearfix = ReactBootstrap.Clearfix;
+var FormControl = ReactBootstrap.FormControl;
+var Navbar = ReactBootstrap.Navbar;
+var Panel = ReactBootstrap.Panel;
+var Table = ReactBootstrap.Table;
 
-    try {
-        var _loop = function _loop() {
-            var plot = _step.value;
+var Navbar_ = function (_React$Component) {
+    _inherits(Navbar_, _React$Component);
 
-            var tr = $('<tr></tr>').appendTo(tbody);
-            $('<td></td>').appendTo(tr).append($('<a></a>').text(plot.comment != '' ? plot.comment : '<no comment>').attr('href', './plot.html?' + plot.id));
-            $('<td></td>').appendTo(tr).attr('class', 'col-md-1').append($('<button></button>').text('Delete').attr('type', 'button').attr('class', 'btn btn-danger').on('click', function () {
-                return monitor.plots.remove(plot).then(update_plots);
-            }));
-        };
+    function Navbar_() {
+        _classCallCheck(this, Navbar_);
 
-        for (var _iterator = monitor.plots[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            _loop();
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
+        return _possibleConstructorReturn(this, (Navbar_.__proto__ || Object.getPrototypeOf(Navbar_)).apply(this, arguments));
     }
-}
 
-function update_logs() {
-    var tbody = $('#logs > tbody');
-    tbody.empty();
-    var _iteratorNormalCompletion2 = true;
-    var _didIteratorError2 = false;
-    var _iteratorError2 = undefined;
-
-    try {
-        var _loop2 = function _loop2() {
-            var log = _step2.value;
-
-            var tr = $('<tr></tr>').appendTo(tbody);
-            $('<td></td>').appendTo(tr).text(log.path);
-            $('<td></td>').appendTo(tr).text(log.comment);
-            $('<td></td>').appendTo(tr).attr('class', 'col-md-1').append($('<button></button>').text('Delete').attr('type', 'button').attr('class', 'btn btn-danger').on('click', function () {
-                return monitor.logs.remove(log).then(update_logs);
-            }));
-        };
-
-        for (var _iterator2 = monitor.logs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            _loop2();
+    _createClass(Navbar_, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                Navbar,
+                null,
+                React.createElement(
+                    Navbar.Header,
+                    null,
+                    React.createElement(
+                        Navbar.Brand,
+                        null,
+                        React.createElement(
+                            'a',
+                            { href: '#' },
+                            'Monitor'
+                        )
+                    )
+                )
+            );
         }
-    } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                _iterator2.return();
-            }
-        } finally {
-            if (_didIteratorError2) {
-                throw _iteratorError2;
-            }
-        }
+    }]);
+
+    return Navbar_;
+}(React.Component);
+
+var PlotList = function (_React$Component2) {
+    _inherits(PlotList, _React$Component2);
+
+    function PlotList(props) {
+        _classCallCheck(this, PlotList);
+
+        var _this2 = _possibleConstructorReturn(this, (PlotList.__proto__ || Object.getPrototypeOf(PlotList)).call(this, props));
+
+        _this2.plots = props.plots;
+        _this2.state = { comment: '' };
+        return _this2;
     }
-}
 
-$(document).ready(function () {
-    monitor.plots.sync().then(update_plots);
-    monitor.logs.sync().then(update_logs);
+    _createClass(PlotList, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var _this3 = this;
 
-    $('#plot-add-button').on('click', function () {
-        var text = $('#comment');
-        monitor.plots.add({ comment: text.val() }).then(function () {
-            update_plots();
-            text.val('');
-        });
-    });
-});
+            this.plots.sync().then(function () {
+                return _this3.setState();
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this4 = this;
+
+            var header = React.createElement(
+                'h3',
+                null,
+                'Plot',
+                React.createElement(
+                    'form',
+                    { className: 'form-inline pull-right' },
+                    React.createElement(FormControl, {
+                        type: 'text',
+                        value: this.state.comment,
+                        placeholder: 'comment',
+                        onChange: function onChange(e) {
+                            return _this4.setState({ comment: e.target.value });
+                        } }),
+                    React.createElement(
+                        Button,
+                        {
+                            bsStyle: 'primary',
+                            onClick: function onClick() {
+                                return _this4.plots.add({ comment: _this4.state.comment }).then(function () {
+                                    return _this4.setState({ comment: '' });
+                                });
+                            }
+                        },
+                        'Add'
+                    )
+                ),
+                React.createElement(Clearfix, null)
+            );
+
+            var rows = [];
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                var _loop = function _loop() {
+                    var plot = _step.value;
+
+                    rows.push(React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            React.createElement(
+                                'a',
+                                { href: './plot.html?' + plot.id },
+                                plot.comment != '' ? plot.comment : '<no comment>'
+                            )
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            React.createElement(
+                                Button,
+                                {
+                                    bsStyle: 'danger',
+                                    bsSize: 'small',
+                                    className: 'pull-right',
+                                    onClick: function onClick() {
+                                        return _this4.plots.remove(plot).then(function () {
+                                            return _this4.setState();
+                                        });
+                                    }
+                                },
+                                'Delete'
+                            )
+                        )
+                    ));
+                };
+
+                for (var _iterator = this.plots[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    _loop();
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+
+            return React.createElement(
+                Panel,
+                { header: header },
+                React.createElement(
+                    Table,
+                    { fill: true },
+                    React.createElement(
+                        'tbody',
+                        null,
+                        rows
+                    )
+                )
+            );
+        }
+    }]);
+
+    return PlotList;
+}(React.Component);
+
+var LogList = function (_React$Component3) {
+    _inherits(LogList, _React$Component3);
+
+    function LogList(props) {
+        _classCallCheck(this, LogList);
+
+        var _this5 = _possibleConstructorReturn(this, (LogList.__proto__ || Object.getPrototypeOf(LogList)).call(this, props));
+
+        _this5.logs = props.logs;
+        return _this5;
+    }
+
+    _createClass(LogList, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var _this6 = this;
+
+            this.logs.sync().then(function () {
+                return _this6.setState();
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this7 = this;
+
+            var header = React.createElement(
+                'h3',
+                null,
+                'Log'
+            );
+
+            var rows = [];
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
+
+            try {
+                for (var _iterator2 = this.logs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                    var log = _step2.value;
+
+                    rows.push(React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'td',
+                            null,
+                            log.path
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            log.comment
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            React.createElement(
+                                Button,
+                                {
+                                    bsStyle: 'danger',
+                                    bsSize: 'small',
+                                    className: 'pull-right',
+                                    onClick: function onClick() {
+                                        return _this7.logs.remove(plot).then(function () {
+                                            return _this7.setState();
+                                        });
+                                    }
+                                },
+                                'Delete'
+                            )
+                        )
+                    ));
+                }
+            } catch (err) {
+                _didIteratorError2 = true;
+                _iteratorError2 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                        _iterator2.return();
+                    }
+                } finally {
+                    if (_didIteratorError2) {
+                        throw _iteratorError2;
+                    }
+                }
+            }
+
+            return React.createElement(
+                Panel,
+                { header: header },
+                React.createElement(
+                    Table,
+                    { fill: true },
+                    React.createElement(
+                        'tbody',
+                        null,
+                        rows
+                    )
+                )
+            );
+        }
+    }]);
+
+    return LogList;
+}(React.Component);
+
+var App = function (_React$Component4) {
+    _inherits(App, _React$Component4);
+
+    function App() {
+        _classCallCheck(this, App);
+
+        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+    }
+
+    _createClass(App, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'container' },
+                React.createElement(Navbar_, null),
+                React.createElement(PlotList, { plots: monitor.plots }),
+                React.createElement(LogList, { logs: monitor.logs })
+            );
+        }
+    }]);
+
+    return App;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
 
 },{"api":1}]},{},[2]);
